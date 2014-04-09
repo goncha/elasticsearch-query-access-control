6c0006756a6966b2633dc35532a8fb1cad0a1425
Gang Chen <goncha@gmail.com>
2014-04-09 17:06:37 +0800

    Update .gitignore

---
+++ src/test/java/org/apache/lucene/search/BaseLuceneAccessControlFilterTest.java
@@ -0,0 +1,11 @@
+package org.apache.lucene.search;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gc
+ * Date: 14-4-9
+ * Time: 下午4:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class BaseLuceneAccessControlFilterTest {
+}
---
+++ src/test/java/org/apache/lucene/search/LuceneAccessFilterProfiler.java
@@ -0,0 +1,11 @@
+package org.apache.lucene.search;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gc
+ * Date: 14-4-9
+ * Time: 下午4:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class LuceneAccessFilterProfiler {
+}
