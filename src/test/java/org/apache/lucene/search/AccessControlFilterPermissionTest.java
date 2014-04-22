42503f9b05716b4cfcffedbf4bc6d0ea702b26ae
Gang Chen <goncha@gmail.com>
2014-04-22 16:27:20 +0800

    Clean dependencies

---
+++ src/test/java/org/apache/lucene/search/AccessControlFilterPermissionTest.java
@@ -0,0 +1,11 @@
+package org.apache.lucene.search;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gc
+ * Date: 14-4-22
+ * Time: 下午4:07
+ * To change this template use File | Settings | File Templates.
+ */
+public class AccessControlFilterPermissionTest {
+}
