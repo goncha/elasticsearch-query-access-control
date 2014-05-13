8776bbc18805510c77f647880acfe4c9f37144c6
Gang Chen <goncha@gmail.com>
2014-05-13 13:45:56 +0800

    Add alternative maven repo example

---
+++ src/test/java/org/apache/lucene/search/AccessControlFilterTest.java
@@ -0,0 +1,11 @@
+package org.apache.lucene.search;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gc
+ * Date: 14-5-13
+ * Time: 下午1:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class AccessControlFilterTest {
+}
