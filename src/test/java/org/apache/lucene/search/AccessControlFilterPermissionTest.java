eeb8adb94682aabe562af904b1c7226166c892c0
Gang Chen <goncha@gmail.com>
2014-05-22 17:24:44 +0800

    Add test class of Permission

--- src/test/java/org/apache/lucene/search/AccessControlFilterPermissionTest.java
+++ src/test/java/org/apache/lucene/search/AccessControlFilterPermissionTest.java
@@ -10,45 +10,59 @@ import java.util.Map;
 public class AccessControlFilterPermissionTest {
 
     @Test
-    public void testNullToString() {
-        Assert.assertEquals("", AccessControlFilter.toString(null));
-    }
-
-    @Test
-    public void testEmptyMapToString() {
-        Assert.assertEquals("", AccessControlFilter.toString(Collections.EMPTY_MAP));
+    public void testEmptyPermissionToString() {
+        Assert.assertEquals("", new Permission().toString());
     }
 
     @Test
     public void testSingleEntryToString() {
-        Map<String,String> p = new HashMap<String,String>();
-        p.put("A", "1");
-        Assert.assertEquals("A=1", AccessControlFilter.toString(p));
+        Permission p = new Permission();
+        p.in("A").add("1");
+        Assert.assertEquals("A=1", p.toString());
     }
 
     @Test
     public void testTwoEntriesToString() {
-        Map<String,String> p = new HashMap<String,String>();
-        p.put("A", "1");
-        p.put("B", "2");
-        Assert.assertEquals("A=1,B=2", AccessControlFilter.toString(p));
+        Permission p = new Permission();
+        p.in("A").add("1");
+        p.in("B").add("2");
+        Assert.assertEquals("A=1;B=2", p.toString());
     }
 
     @Test
     public void testTwoEntriesInReverseOrderToString() {
-        Map<String,String> p = new HashMap<String,String>();
-        p.put("B", "2");
-        p.put("A", "1");
-        Assert.assertEquals("A=1,B=2", AccessControlFilter.toString(p));
+        Permission p = new Permission();
+        p.in("B").add("2");
+        p.in("A").add("1");
+        Assert.assertEquals("A=1;B=2", p.toString());
     }
 
     @Test
     public void testThreeEntriesInReverseOrderToString() {
-        Map<String,String> p = new HashMap<String,String>();
-        p.put("C", "3");
-        p.put("B", "2");
-        p.put("A", "1");
-        Assert.assertEquals("A=1,B=2,C=3", AccessControlFilter.toString(p));
+        Permission p = new Permission();
+        p.in("C").add("3");
+        p.in("B").add("2");
+        p.in("A").add("1");
+        Assert.assertEquals("A=1;B=2;C=3", p);
+    }
+
+    @Test
+    public void testMultiItemsInOneEntryToString() {
+        Permission p = new Permission();
+        p.in("A").add("3").add("2").add("1");
+        Assert.assertEquals("A=1,2,3", p.toString());
+    }
+
+    @Test
+    public void testMultiItemsInMultiEntriesToString() {
+        Permission p = new Permission();
+        p.in("B").add("2");
+        p.in("A").add("1");
+        p.in("B").add("3");
+        p.in("A").add("3");
+        p.in("B").add("1");
+        p.in("A").add("2");
+        Assert.assertEquals("A=1,2,3;B=1,2,3", p.toString());
     }
 
     @Test
